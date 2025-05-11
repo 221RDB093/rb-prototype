57b461f69d2934644b05b186a3eb3a44bd5ab2e6
221RDB093 <221RDB093@users.noreply.github.com>
2025-05-11 19:55:17 +0300

    Authorization layout adjustments

--- rb-prototype/src/pages/Authorization.jsx
+++ rb-prototype/src/pages/Authorization.jsx
@@ -1,17 +1,23 @@
 import '../styles/Authorization.css'
+import google from '../assets/googleLogo1.png'
 
 function Authorization() { 
     return (
         <>
+            <h1>Autorizācija</h1>
             <div className="card">
                 <label for="username">Lietotājvārds</label><br/>
-                <input type="text" placeholder="Lietotājvārds" id="username" className="userName"/>
+                <input type="text" placeholder="Lietotāja e-pasts" id="username" className="userName"/>
 
                 <label for="password">Parole</label><br/>
                 <input type="text" placeholder="Parole" id="password" className="userName"/>
 
+               <a href="/" class="google">
+                    <img src={google} className="google-icon"></img>
+                    <p>Turpināt ar Google kontu</p>
+                </a>
                 <div className="btns">
-                    <button className="auth">Autorizēties</button>
+                    <button className="auth">Autorizēties</button><br/>
                     <button className="auth">Reģistrēties</button>
                 </div>
             </div>
